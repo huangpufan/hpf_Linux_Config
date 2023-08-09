288d3d4fd3c2c27bb263cd9f447a2a5a7642f26f
huangpufan <59730801@qq.com>
2023-08-09 12:22:24 +0800

    update to tar

--- pull_nvim_config.sh
+++ pull_nvim_config.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
-cp -r ./neovim/config_nvim ~/.config/nvim 
-cp -r ./neovim/local_share_nvim ~/.local/share/nvim
+
+# 压缩文件路径
+archive_file_1="./neovim/config_nvim.gz"
+archive_file_2="./neovim/local_share_nvim.gz"
+
+# 目标解压目录
+target_folder_1="~/.config/nvim"
+target_folder_2="~/.local/share/nvim"
+# 解压缩文件
+tar -xzf "$archive_file_1" -C "$target_folder_1"
+tar -xzf "$archive_file_2" -C "$target_folder_2"
