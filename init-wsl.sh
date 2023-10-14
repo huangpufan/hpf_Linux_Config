4ef3340efb651f65280d5ac2776780f32e5d7d28
huangpufan <59730801@qq.com>
2023-10-15 00:28:42 +0800

    调整文件夹

---
+++ init-wsl.sh
@@ -0,0 +1,97 @@
+BASHRC = "~/.bashrc"
+ 
+ 
+# Step 1 换源
+ 
+mv /etc/apt/sources.list /etc/apt/sources.list.backup
+ 
+content='# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse'
+ 
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse"  
+ 
+filename="/etc/apt/sources.list"   
+# 将内容写入文件
+echo "$content" > "$filename"
+sudo apt update
+sudo apt upgrade
+# 输出成功消息
+echo "换源成功：$filename"
+ 
+ 
+#Step 2 编辑 .bashrc 文件
+content=$(cat <<'EOF'
+alias eb='nvim ~/.bashrc'
+alias sb='source ~/.bashrc'
+export hostip=\$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')
+alias setss='export all_proxy=\"socks5://\${hostip}:7890\";'
+alias unsetss='unset all_proxy''
+EOF
+) 
+ 
+echo -e $content >> $BASHRC
+echo "bashrc 更新成功"
+ 
+ 
+# Step 2.5 git ssh key
+ 
+ 
+ 
+# Step 3 nvim 相关
+sudo apt -y install -y gcc wget iputils-ping python3-pip git bear tig shellcheck ripgrep
+sudo apt -y install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+sudo apt -y install efm-langserver ccls npm cargo xclip lua5.4
+sudo snap install marksman
+git clone --depth=1 https://github.com/neovim/neovim && cd neovim
+make CMAKE_BUILD_TYPE=Release -j8
+sudo make install
+ 
+ 
+# Step 4 各类基础软件
+sudo apt -y install htop
+# zoxide 
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+content=$(cat <<'EOF'
+eval "$(mcfly init bash)"
+alias cd="z"
+EOF 
+) 
+echo -e $content>> $BASHRC
+# bat
+echo "Bat install start."
+cargo install --locked bat
+echo "Bat install success!"
+# lazygit
+# cd ~/download/
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm -rf lazygit* && cd
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+echo -e "alias f=\"fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'\"" >> $BASHRC
+
+# tmux
+sudo apt -y install tmux
+echo -e "alias a=\"tmux a -t\"" >>  ~/.bashrc
+
+# cgdb
+cd download && git clone git://github.com/cgdb/cgdb.git --depth=1 && cd cgdb
+./autogen.sh
+./configure
+make -j
+make install
+cd && mkdir ~/.cgdb &&cd .cgdb
+
+
