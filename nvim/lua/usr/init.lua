79f8e8b592a365ee2f3b097887ac4c15e227cbe7
huangpufan <59730801@qq.com>
2024-02-05 12:30:44 +0800

    Add hlargs.nvim
    Paint the args with color.

--- nvim/lua/usr/init.lua
+++ nvim/lua/usr/init.lua
@@ -13,7 +13,7 @@ require("usr.version")
 require("usr.which-key")
 require("usr.colorscheme")
 require("usr.alpha")
-require("colorizer").setup({ "css", "javascript", "vim","lua", html = { mode = "foreground" } })
+require("colorizer").setup({ "css", "javascript", "vim", "lua", html = { mode = "foreground" } })
 require("nvim-surround").setup()
 require("persisted").setup({ autoload = true })
 require("gitsigns").setup({ signcolumn = false, numhl = true })
@@ -49,75 +49,113 @@ require("lsp_signature").setup()
 
 -- workaround for https://github.com/neovim/neovim/issues/21856
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
-  callback = function()
-    vim.cmd("sleep 10m")
-  end,
+	callback = function()
+		vim.cmd("sleep 10m")
+	end,
 })
 
 require("aerial").setup({
-  backends = { "markdown", "man", "lsp", "treesitter" },
-  layout = {
-    max_width = { 30, 0.15 },
-    placement = "edge",
-    default_direction = "left",
-  },
-  attach_mode = "global",
+	backends = { "markdown", "man", "lsp", "treesitter" },
+	layout = {
+		max_width = { 30, 0.15 },
+		placement = "edge",
+		default_direction = "left",
+	},
+	attach_mode = "global",
 })
 
 require("bookmarks").setup({
-  mappings_enabled = false,
-  virt_pattern = { "*.lua", "*.md", "*.c", "*.h", "*.sh" },
+	mappings_enabled = false,
+	virt_pattern = { "*.lua", "*.md", "*.c", "*.h", "*.sh" },
 })
 
 -- require("tabout").setup()
 require("goto-preview").setup({
-  default_mappings = true,
+	default_mappings = true,
 })
 -- require("ibl").setup()
 require("mini.indentscope").setup()
 -- instances.
 require("neoscroll").setup()
-require('copilot').setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
-    },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    debounce = 75,
-    keymap = {
-      accept = "<M-l>",
-      accept_word = false,
-      accept_line = false,
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    markdown = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-    cpp = true,
-    c=true,
-    bash=true,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 18.x
-  server_opts_overrides = {},
+require("copilot").setup({
+	panel = {
+		enabled = true,
+		auto_refresh = false,
+		keymap = {
+			jump_prev = "[[",
+			jump_next = "]]",
+			accept = "<CR>",
+			refresh = "gr",
+			open = "<M-CR>",
+		},
+		layout = {
+			position = "bottom", -- | top | left | right
+			ratio = 0.4,
+		},
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		debounce = 75,
+		keymap = {
+			accept = "<M-l>",
+			accept_word = false,
+			accept_line = false,
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		markdown = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+		cpp = true,
+		c = true,
+		bash = true,
+	},
+	copilot_node_command = "node", -- Node.js version must be > 18.x
+	server_opts_overrides = {},
+})
+
+require("hlargs").setup({
+	color = "#ef9062",
+	highlight = {},
+	excluded_filetypes = {},
+	-- disable = function(lang, bufnr) -- If changed, `excluded_filetypes` will be ignored
+	-- 	return vim.tbl_contains(opts.excluded_filetypes, lang)
+	-- end,
+	paint_arg_declarations = true,
+	paint_arg_usages = true,
+	paint_catch_blocks = {
+		declarations = false,
+		usages = false,
+	},
+	extras = {
+		named_parameters = false,
+	},
+	hl_priority = 10000,
+	excluded_argnames = {
+		declarations = {},
+		usages = {
+			python = { "self", "cls" },
+			lua = { "self" },
+		},
+	},
+	performance = {
+		parse_delay = 1,
+		slow_parse_delay = 50,
+		max_iterations = 400,
+		max_concurrent_partial_parses = 30,
+		debounce = {
+			partial_parse = 3,
+			partial_insert_mode = 100,
+			total_parse = 700,
+			slow_parse = 5000,
+		},
+	},
 })
--- nvim/lua/usr/lazy.lua
+++ nvim/lua/usr/lazy.lua
@@ -315,19 +315,5 @@ require("lazy").setup({
     build = ":Copilot auth",
   },
   { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true,opt={background = light,}},
---  {
---  "folke/noice.nvim",
---  event = "VeryLazy",
- -- opts = {
---    -- add any options here
---  },
---  dependencies = {
---    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
---    "MunifTanjim/nui.nvim",
---    -- OPTIONAL:
---    --   `nvim-notify` is only needed, if you want to use the notification view.
---    --   If not available, we use `mini` as the fallback
---    "rcarriga/nvim-notify",
---    }
---},
+  "m-demare/hlargs.nvim",
 }, {})
