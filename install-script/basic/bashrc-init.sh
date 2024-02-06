9853c9f8c69ef159f2fdd8f2155410143d6513a1
huangpufan <59730801@qq.com>
2024-02-07 00:06:19 +0800

    Add idempotence to bashrc.sh.

--- install-script/basic/bashrc-init.sh
+++ install-script/basic/bashrc-init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #
-# Add the bashrc-append to the end of ~/.bashrc
-cat ./bashrc-append >> ~/.bashrc
-source ~/.bashrc
-export hostip=$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')
-export all_proxy="socks5://${hostip}:7890";
+# Check if the Bashrc Already Set string exists in the .bashrc file
+if grep -q "Bashrc Already Set" ~/.bashrc; then
+    echo "Bashrc already set. Skipping script execution."
+else
+    # Add the bashrc-append to the end of ~/.bashrc
+    cat ./bashrc-append >> ~/.bashrc
+    source ~/.bashrc
+    export hostip=$(cat /etc/resolv.conf | grep -oP '(?<=nameserver\ ).*')
+    export all_proxy="socks5://${hostip}:7890"
+fi
