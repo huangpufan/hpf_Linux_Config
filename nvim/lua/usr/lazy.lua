22dbb0ca95b38478b46d3b5c902c098946c89852
huangpufan <59730801@qq.com>
2024-02-06 14:50:01 +0800

    Mason set ensure install lsp.

--- nvim/lua/usr/lazy.lua
+++ nvim/lua/usr/lazy.lua
@@ -43,7 +43,21 @@ require("lazy").setup({
 
   -- LSP
   { "neovim/nvim-lspconfig" },           -- enable LSP
-  { "williamboman/mason.nvim" },         -- simple to use language server installer
+  { "williamboman/mason.nvim",
+   cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+   opts = function()
+      return require "usr.mason"
+    end,
+    config = function(_, opts)
+      require("mason").setup(opts)
+      vim.api.nvim_create_user_command("MasonInstallAll", function()
+        if opts.ensure_installed and #opts.ensure_installed > 0 then
+          vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+        end
+      end, {})
+      vim.g.mason_binaries_list = opts.ensure_installed
+    end,
+},         -- simple to use language server installer
   { "williamboman/mason-lspconfig.nvim" },
   { "jose-elias-alvarez/null-ls.nvim" }, -- for formatters and linters
   { "j-hui/fidget.nvim",                tag = "legacy" },
