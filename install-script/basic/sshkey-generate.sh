7d61effcce730a733ac88c6e09eb8b4c253bc92f
huangpufan <59730801@qq.com>
2024-02-06 20:48:32 +0800

    ssh-generate show github-link:ssh key add.

--- install-script/basic/sshkey-generate.sh
+++ install-script/basic/sshkey-generate.sh
@@ -1,7 +1,10 @@
 BASHRC="/home/$(whoami)/.bashrc"
 GIT_EMAIL="59730801@qq.com"
 GIT_NAME="huangpufan"
-
-echo -e '\n' | ssh-keygen -t ed25519 -C $GIT_EMAIL
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    echo -e '\n' | ssh-keygen -t ed25519 -C $GIT_EMAIL
+fi
 git config --global user.name $GIT_NAME
 git config --global user.email $GIT_EMAIL
+echo '\n'
+echo 'Enter: https://github.com/settings/ssh/new'
--- install-script/basic/ubuntu-source-change.sh
+++ install-script/basic/ubuntu-source-change.sh
@@ -8,10 +8,10 @@ ubuntu_version=$(cat /etc/os-release | grep VERSION_ID | cut -d '=' -f 2 | tr -d
 # According to the version of Ubuntu, copy the corresponding source list to /etc/apt/sources.list
 if [[ "$ubuntu_version" == "22.04" ]]; then
   print_with_padding "This is ubuntu 22.04 verison"
-  sudo cp ./sourcelist-for-ubuntu2204 /etc/apt/sources.list
+  sudo cp ./source-2204/etc/apt/sources.list
 elif  [[ "$ubuntu_version" == "20.04" ]]; then
   print_with_padding "This is ubuntu 20.04 verison"
-  sudo cp ./sourcelist-for-ubuntu2004 /etc/apt/sources.list
+  sudo cp ./source-2004/etc/apt/sources.list
 else
   print_with_padding "!!! This Configuration is not prepared for your ubuntu version.Terminated."
   exit 1
