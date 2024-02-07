fa8d2732b2088f35c28d21517dc44b591f669ea9
huangpufan <59730801@qq.com>
2024-02-07 09:00:39 +0800

    Add idempotence to curl/cargo install.

--- install-script/basic/cargo-install.sh
+++ install-script/basic/cargo-install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if command -v cargo &> /dev/null; then
+  echo "cargo is already installed. Exiting..."
+  exit 0
+fi
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 cargo install stylua
--- install-script/basic/curl-install.sh
+++ install-script/basic/curl-install.sh
@@ -1,11 +1,20 @@
+# Check if zoxide is installed and install it if it is not
+if ! command -v zoxide >/dev/null 2>&1; then
+    echo "zoxide not found, installing..."
+    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+else
+    echo "zoxide is already installed."
+fi
 
-# Install zoxide
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-
-# Install lazygit
-cd ~/download/
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-rm -rf lazygit* && cd
+# Check if lazygit is installed and install it if it is not
+if ! command -v lazygit >/dev/null 2>&1; then
+    echo "lazygit not found, installing..."
+    mkdir -p ~/download && cd ~/download/
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+    rm -rf lazygit* && cd
+else
+    echo "lazygit is already installed."
+fi
