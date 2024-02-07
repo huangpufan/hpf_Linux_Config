b7397509d8ae44f735111195e4122db64f30bdf4
huangpufan <59730801@qq.com>
2024-02-07 16:18:07 +0800

    Make source-change.sh feasible

--- install-script/basic/ubuntu-source-change.sh
+++ install-script/basic/ubuntu-source-change.sh
@@ -14,10 +14,10 @@ ubuntu_version=$(cat /etc/os-release | grep VERSION_ID | cut -d '=' -f 2 | tr -d
 # According to the version of Ubuntu, copy the corresponding source list to /etc/apt/sources.list
 if [[ "$ubuntu_version" == "22.04" ]]; then
     print_with_padding "This is Ubuntu 22.04 version"
-    sudo cp ./source-2204/etc/apt/sources.list /etc/apt/sources.list
+    sudo cp ./source-2204  /etc/apt/sources.list
 elif [[ "$ubuntu_version" == "20.04" ]]; then
     print_with_padding "This is Ubuntu 20.04 version"
-    sudo cp ./source-2004/etc/apt/sources.list /etc/apt/sources.list
+    sudo cp ./source-2004 /etc/apt/sources.list
 else
     print_with_padding "!!! This configuration is not prepared for your Ubuntu version. Terminated."
     exit 1
