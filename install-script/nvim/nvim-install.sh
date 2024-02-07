8581c227c6ec35656fd050d55fe2a2aa12198dc6
huangpufan <59730801@qq.com>
2024-02-07 18:01:10 +0800

    Make nvim-install.sh feasible

--- install-script/nvim/nvim-install.sh
+++ install-script/nvim/nvim-install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+setss
 # Basic dependencies install
-sudo apt -y install -y gcc wget iputils-ping python3-pip git bear tig shellcheck ripgrep
-sudo apt -y install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-sudo apt -y install ccls npm cargo xclip
+sudo apt -y install gcc wget iputils-ping python3-pip git bear tig 
+sudo apt -y install ninja-build gettext libtool libtool-bin autoconf 
+sudo apt -y install automake cmake g++ pkg-config unzip curl doxygen
+sudo apt -y install ccls npm cargo xclip shellcheck ripgrep
 # sudo snap install marksman --classic
 # sudo snap install pyright --classic
 # cp ./../install_package/marksman-linux-x64 ~/.local/bin/
-sudo npm install -g vim-language-server
+# sudo npm install -g vim-language-server
 pip3 install --user pynvim
 if [[ $ubuntu_version == "22.04" ]] ; then
   sudo apt -y install efm-langserver lua5.4
@@ -15,22 +17,18 @@ fi
 
 
 # Latest version of neovim install
-cd ~
-rm -rf ./neovim
-git clone --depth=1 https://github.com/neovim/neovim && cd neovim
+rm -rf ~/download/neovim
+git clone --depth=1 https://github.com/neovim/neovim ~/download/neovim 
+cd ~/download/neovim
 make CMAKE_BUILD_TYPE=Release -j32
 sudo make install
-cd ~
-
-
 # Remove the tmp folder
-rm -rf ./neovim
+rm -rf ~/download/neovim
 
 
 # Clear the old nvim config
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim/
-
-
 # Link the new nvim config
 ln -s ~/hpf_Linux_Config/nvim ~/.config/nvim
+./clipboard-prepare.sh
