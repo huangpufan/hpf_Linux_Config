b756abdedacbea8d7584417a59cfbe6abdf5ea12
huangpufan <59730801@qq.com>
2024-02-12 19:36:28 +0800

    Set harpoon keymap.

--- nvim/lua/usr/init.lua
+++ nvim/lua/usr/init.lua
@@ -282,3 +282,41 @@ require("project_nvim").setup({
 	-- or leave it empty to use the default settings
 	-- refer to the configuration section below
 })
+
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<A-a>", function() harpoon:list():append() end)
+vim.keymap.set("n", "<A-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<A-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<A-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<A-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<A-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+-- basic telescope configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<A-e>", function() toggle_telescope(harpoon:list()) end,
+    { desc = "Open harpoon window" })
--- nvim/lua/usr/lazy.lua
+++ nvim/lua/usr/lazy.lua
@@ -481,4 +481,5 @@ require("lazy").setup({
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
+
 }, {})
