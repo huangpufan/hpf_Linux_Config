cf026d6a17a10bd4fcb14c4fd63ccef1684f1a2c
huangpufan <59730801@qq.com>
2024-02-16 08:32:43 +0800

    Add tmux completion.

--- install-script/tmux/install-tmux-config.sh
+++ install-script/tmux/install-tmux-config.sh
@@ -1,1 +1,6 @@
+# Set tmux configuration
 ln -s ~/hpf_Linux_Config/install-script/tmux/tmux.conf ~/.tmux.conf
+
+# Set tmux completion
+cp ./tmux-completion /etc/bash_completion.d/tmux-completion
+source /etc/bash_completion.d/tmux-completion
