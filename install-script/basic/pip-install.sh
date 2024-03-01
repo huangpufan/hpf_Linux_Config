e2bbf6524bda917420280d18c3450d01448e97aa
huangpufan <59730801@qq.com>
2024-03-01 14:19:54 +0800

    Add ueberzug install by pip3
    A x11 related tool for picture show in terminals.

--- install-script/basic/pip-install.sh
+++ install-script/basic/pip-install.sh
@@ -5,5 +5,6 @@ if python -c "import pysocks" >/dev/null 2>&1; then
     echo "pysocks is already installed."
 else
     # Install pysocks
-    pip install pysocks
+    pip install pysocks -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+    pip3 install --user ueberzug -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 fi
