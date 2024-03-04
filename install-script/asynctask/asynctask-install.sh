137bc9d4d53ce81581efec9ccd437a8e8dd1d6fe
huangpufan <59730801@qq.com>
2024-03-04 09:14:11 +0800

    Add asynctask install

---
+++ install-script/asynctask/asynctask-install.sh
@@ -0,0 +1,3 @@
+git clone --depth 1 https://github.com/skywind3000/asynctasks.vim ~/install/asynctasks.vim
+ln -s ~/install/asynctasks.vim/bin/asynctask ~/bin
+ln -s ~/hpf_Linux_Config/install-script/asynctask/tasks.ini ~/.config/nvim/tasks.ini
