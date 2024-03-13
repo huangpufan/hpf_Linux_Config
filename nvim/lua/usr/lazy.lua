667512b246c47553a81f210f834563b3bc7ecd42
huangpufan <59730801@qq.com>
2024-03-13 10:29:35 +0800

    Flash.nvim add a space

--- nvim/lua/usr/lazy.lua
+++ nvim/lua/usr/lazy.lua
@@ -268,18 +268,43 @@ require("lazy").setup({
     lazy = false,
     event = "VeryLazy",
     -- @type Flash.Config
-    opts = {},
-    -- stylua: ignore
+    opts = {
+      label = {
+        after = { 0, 2 },
+      },
+    }, -- stylua: ignore
     keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
       {
         "R",
         mode = { "o", "x" },
-        function() require("flash").treesitter_search() end,
-        desc =
-        "Treesitter Search"
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
       },
     },
   },
