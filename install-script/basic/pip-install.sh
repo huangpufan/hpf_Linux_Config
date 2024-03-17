6e6585dcd8f86e9f4a025730b1d1ef57f077e59a
huangpufan <59730801@qq.com>
2024-03-17 11:32:31 +0800

    Add gdbfrontend install

--- install-script/basic/pip-install.sh
+++ install-script/basic/pip-install.sh
@@ -7,4 +7,12 @@ else
     # Install pysocks
     pip install pysocks -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
     pip3 install --user ueberzug -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+ 
+    sudo python3 -m pip install gdbfrontend
+    
+    # Install gdbgui
+    python3 -m pip install --user pipx
+    sudo apt install python3.8-venv
+    pipx install gdbgui --force
+
 fi
