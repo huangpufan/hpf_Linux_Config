b27d2c65c065244d99679d3ed5884a949a8ab8a7
huangpufan <59730801@qq.com>
2024-03-19 17:32:34 +0800

    Add cargo config to use QingHua source

--- install-script/basic/cargo-install.sh
+++ install-script/basic/cargo-install.sh
@@ -7,7 +7,7 @@ fi
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
-
+ln -s "$HOME/hpf_Linux_Config/install-script/basic/cargo-config" "$HOME/.cargo/config"
 cargo install mprocs eza sd
 cargo install --locked ouch
 cargo install --locked yazi-fm
