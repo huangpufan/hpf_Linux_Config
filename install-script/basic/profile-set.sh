b3957a69d831647c6726465fbf4d57c3507d1a67
huangpufan <59730801@qq.com>
2024-03-20 10:09:42 +0800

    Fix profile set script

--- install-script/basic/profile-set.sh
+++ install-script/basic/profile-set.sh
@@ -4,15 +4,15 @@
 PROFILE_FILE="$HOME/.profile"
 BASHRC_FILE="$HOME/.bashrc"
 
-# Define the string to look for and the string to add
-SEARCH_STRING="source ~/.bashrc"
-ADD_STRING="if [ -s $BASHRC_FILE ]; then\n    source $BASHRC_FILE;\nfi"
+# Define the new string to look for and the string to add
+SEARCH_STRING="Profile already set"
+ADD_STRING="if [ -s $BASHRC_FILE ]; then\n    source $BASHRC_FILE\nfi\n# Profile already set"
 
-# Check if the line exists in the profile file
+# Check if the string exists in the profile file
 if ! grep -qF "$SEARCH_STRING" "$PROFILE_FILE"; then
-  # If the line doesn't exist, append the conditional source block
+  # If the string doesn't exist, append the conditional source block and the marker
   echo -e "$ADD_STRING" >> "$PROFILE_FILE"
-  echo "The .bashrc source block was added to your .profile."
+  echo "The conditional .bashrc source block and marker were added to your .profile."
 else
-  echo "Your .profile already sources .bashrc."
+  echo "Your .profile already has the marker (and presumably sources .bashrc)."
 fi
