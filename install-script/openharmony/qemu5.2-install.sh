9860c9909a37eb647f332a57cd96b3b79b5397a4
huangpufan <59730801@qq.com>
2024-04-20 10:47:22 +0800

    [OPTIMIZE] Optimize the qemu install script

--- install-script/openharmony/qemu5.2-install.sh
+++ install-script/openharmony/qemu5.2-install.sh
@@ -10,7 +10,19 @@ cd build
 ../configure --prefix=/home/hpf/install/qemu-5.2.0/qemugiett/qemu/
 make -j32
 make install
-echo ""
-echo "ADD   export PATH=\$PATH:/home/hpf/install/qemu-5.2.0/qemugiett/qemu/  to your bashrc!"
-echo "And source ~/.bashrc"
+
+
+# 将需要添加的路径设置为一个变量
+PATH_TO_ADD='export PATH=$PATH:/home/hpf/install/qemu-5.2.0/qemugiett/qemu/bin'
+
+# 检查 ~/.bashrc 文件中是否已包含该路径
+if ! grep -qF "$PATH_TO_ADD" ~/.bashrc; then
+  # 如果没有找到，添加到 ~/.bashrc 文件末尾
+  echo "$PATH_TO_ADD" >> ~/.bashrc
+  echo "Path has been added to ~/.bashrc"
+else
+  echo "Path already exists in ~/.bashrc"
+fi
+
+echo "Please source ~/.bashrc"
 rm -rf ~/download/qemu
