53dc75a5f647a5ab198cab710d0097e204d6368f
huangpufan <59730801@qq.com>
2024-09-13 22:58:44 +0800

    [remove] remove markview for now
    Same: nvim version question

--- nvim/lua/usr/lazy.lua
+++ nvim/lua/usr/lazy.lua
@@ -514,18 +514,18 @@ require("lazy").setup({
     -- event = { "InsertEnter" },
     event = { "VeryLazy" },
   },
-  {
-    "OXY2DEV/markview.nvim",
-    -- lazy = false,      -- Recommended
-    ft = "markdown", -- If you decide to lazy-load anyway
+--   {
+--     "OXY2DEV/markview.nvim",
+--     -- lazy = false,      -- Recommended
+--     ft = "markdown", -- If you decide to lazy-load anyway
 
-    dependencies = {
-        -- You will not need this if you installed the
-        -- parsers manually
-        -- Or if the parsers are in your $RUNTIMEPATH
-        "nvim-treesitter/nvim-treesitter",
+--     dependencies = {
+--         -- You will not need this if you installed the
+--         -- parsers manually
+--         -- Or if the parsers are in your $RUNTIMEPATH
+--         "nvim-treesitter/nvim-treesitter",
 
-        "nvim-tree/nvim-web-devicons"
-    }
-},
+--         "nvim-tree/nvim-web-devicons"
+--     }
+-- },
 }, {})
